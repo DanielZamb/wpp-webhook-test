b8f06c96c75f37ab022bc706b31201731573d152
Daniel Zambrano <DanielZamb@uniandes.edu.co>
2025-10-30 15:51:08 -0500

    server

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+app.get("/", (req, res) => {
+  const {
+    "hub.mode": module,
+    "hub.challenge": challenge,
+    "hub.verify_token": token,
+  } = req.query;
+
+  if (mode === "subscribe" && token === verifyToken) {
+    console.log("WEBHOOK VERIFIED");
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
+});
+
+app.post("/", (req, res) => {
+  const timestamp = new Date().toISOString().replace("T", " ").slice(0, 19);
+  console.log(`\n Webhook received at ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
